ccfc90cdf2b37eb6dbb9a2e0a91fd86f95efdebb
GDDPR <kevinzhengofficial@gmail.com>
2026-02-20 17:40:23 -0500

    implemented docker compose

--- run.py
+++ run.py
@@ -1,9 +1,17 @@
 import subprocess
 import sys
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
 
 def main() -> None:
     # Equivalent to: streamlit run app.py
-    cmd = [sys.executable, "-m", "streamlit", "run", "app.py"]
+    cmd = [sys.executable, "-m", "streamlit", "run", "app.py",
+        "--server.address=0.0.0.0",
+        "--server.port=8501",
+        "--server.headless=true",
+    ]
     raise SystemExit(subprocess.call(cmd))
 
 if __name__ == "__main__":
